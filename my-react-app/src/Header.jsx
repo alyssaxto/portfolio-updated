d5620f55ea90d21ee273fa5647dd9646a3519a63
Alyssa To <142935267+alyssaxto@users.noreply.github.com>
2025-02-21 16:22:06 -0600

    started new react

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+
+    return(
+        <header>
+
+            <h1>My website</h1>
+            <nav>
+                
+            </nav>
+        </header>
+    );
+
+}
+
+export default Header
