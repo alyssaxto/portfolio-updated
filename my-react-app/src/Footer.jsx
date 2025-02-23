7ddff46fd378458ca0d1ad7e8a2c4bf503e80ab5
Alyssa To <142935267+alyssaxto@users.noreply.github.com>
2025-02-23 17:39:22 -0600

    footeer basics

--- my-react-app/src/Footer.jsx
+++ my-react-app/src/Footer.jsx
@@ -1,9 +1,24 @@
-function Footer(){
-return(
-    <footer>
-    <p>&copy; Alyssa To {new Date().getFullYear()}</p>
-    <p>Made with ♡ and 🍵</p>
-    </footer>
-);
-}
-export default Footer
+import "./Footer.css";
+function Footer() {
+    return (
+      <footer className="footer">
+        <div className="footer__left">
+          <h2>Thanks for stopping by!</h2>
+          <p>&copy; Alyssa To {new Date().getFullYear()}</p>
+          <p>Made with ♡ and 🍵</p>
+        </div>
+        <div className="footer__right">
+          <h3>Alternatively, here’s a fancy sheet of paper, </h3>
+            <h3><a href="/resume.pdf" target="_blank" rel="noopener noreferrer">Resume ↗</a></h3>
+          <div className="footer__links">
+            <p><a href="https://www.linkedin.com/in/alyssaxto" target="_blank" rel="noopener noreferrer">Linkedin ↗&nbsp;&nbsp;&nbsp;</a>
+            <a href="mailto:alyssaxto@gmail.com">Email ↗&nbsp;&nbsp;&nbsp;</a>
+            <a href="https://github.com/alyssaxto" target="_blank" rel="noopener noreferrer">GitHub ↗&nbsp;&nbsp;&nbsp;</a></p>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
