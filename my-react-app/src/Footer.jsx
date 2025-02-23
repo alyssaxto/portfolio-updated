5a60d1cfbd060885614e9bd196787db48f4de348
Alyssa To <142935267+alyssaxto@users.noreply.github.com>
2025-02-23 14:45:31 -0600

    basic stuffs

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+return(
+    <footer>
+    <p>Copyrighted hehe</p>
+    </footer>
+);
+}
+export default Footer
--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -2,14 +2,17 @@ function Header(){
 
     return(
         <header>
-
-            <h1>My website</h1>
+            <h1>My portfolio</h1>
             <nav>
-                
+                <ul>
+                    <li><a href="#">WORK</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">PLAY</a></li>
+                    <li><a href="#">RESUME</a></li>
+                </ul>
             </nav>
         </header>
     );
-
 }
 
 export default Header
