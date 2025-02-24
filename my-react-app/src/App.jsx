07ea6671aab25882d5c0fa3a1cc754e556176a1f
Alyssa To <142935267+alyssaxto@users.noreply.github.com>
2025-02-23 19:11:09 -0600

    photos added

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './Header.jsx'
 import Hero from './Hero.jsx'
 import Footer from './Footer.jsx'
+import Projects from './projects.jsx'
 function App() {
 
     return(
         <>
         <Header/>
         <Hero/>
+        <Projects/>
         <Footer/>
         </>
     );
