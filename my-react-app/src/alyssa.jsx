547bfe792d1abfa558776d748d89de10a5db0f6d
Alyssa To <142935267+alyssaxto@users.noreply.github.com>
2025-03-03 12:43:09 -0600

    made stuff more mobile friendly

--- my-react-app/src/alyssa.jsx
+++ my-react-app/src/alyssa.jsx
@@ -8,15 +8,15 @@ const AboutAndGallery = () => {
   return (
     <div>
       <div className="about" id="about">
-        <StaggeredFade text="A bit about me ♡‧₊˚" className="about-heading" />
         <div className="about__container">
           <div className="about__img--card">
             <img src={Profile} alt="Image of Alyssa" />
           </div>
           <div className="about__content">
             <TextFade direction="up">
+              <h1>Hi, I'm Alyssa!</h1>
               <p>
-                Nice to meet you, <a href="https://www.linkedin.com/in/alyssaxto/" className="pink-link" target="_blank" rel="noopener noreferrer">I'm Alyssa!</a> I’m a recent graduate from <a href="https://bass.utdallas.edu/degrees/undergraduate-degrees/arts-technology-and-emerging-communication/" className="pink-link" target="_blank" rel="noopener noreferrer">the University of Texas @ Dallas</a> with a Bachelors of Arts, Technology, and Emerging Communications and a concentration on <a href="index.html#Projects" className="pink-link">UX Design.</a><br /><br />
+                Nice to meet you! I’m a recent graduate from <a href="https://bass.utdallas.edu/degrees/undergraduate-degrees/arts-technology-and-emerging-communication/" className="pink-link" target="_blank" rel="noopener noreferrer">the University of Texas @ Dallas</a> with a Bachelors of Arts, Technology, and Emerging Communications and a concentration on <a href="index.html#Projects" className="pink-link">UX Design.</a><br /><br />
               </p>
               <p>
                 I come from a background of fine art but fell in love with UI/UX after discovering it in college. Unlike traditional art, it was goal-driven and functional, which deeply resonated with me.
@@ -28,6 +28,7 @@ const AboutAndGallery = () => {
         <StaggeredFade text="Outside of design..." className="about-heading" />
 
         <div className="gallery-container">
+        <TextFade direction="up">
   <div className="gallery-wrapper">
     <div className="gallery-item">
       <img src="./src/assets/me.png" alt="Making YouTube videos" className="gallery-image" />
@@ -42,6 +43,7 @@ const AboutAndGallery = () => {
       <h2 className="gallery-caption">Attending Hackathons</h2>
     </div>
   </div>
+  </TextFade>
 </div>
 
       </div>
