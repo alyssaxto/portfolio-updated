9c906c673f59ebf502e4844388080515994bc6c2
Alyssa To <142935267+alyssaxto@users.noreply.github.com>
2025-03-04 19:21:59 -0600

    editing root??

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/alyssaxto.github.io/',  // This is correct for GitHub Pages
   resolve: {
     alias: {
       '@': '/src',  // Ensures @ points to the src directory
-    }
-  }
+    },
+  },
 });
