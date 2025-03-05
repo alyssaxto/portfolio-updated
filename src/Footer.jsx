f6ec6bc74fc7308e1d6d36f0e401c4a3ced6a59c
Alyssa To <142935267+alyssaxto@users.noreply.github.com>
2025-03-05 13:45:22 -0600

    emojiss

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -10,19 +10,25 @@ function Footer() {
             <div className="footer__left">
                 <h22>Thanks for stopping by!</h22>
                 <p>&copy; Alyssa To {new Date().getFullYear()}</p>
-                <p>Made with ♡ and 🍵</p>
+                <p>Made with <span className="emoji">♡</span> and <span className="emoji">🍵</span></p>
             </div>
             <div className="footer__right">
                 <h3>Alternatively, here’s a fancy sheet of paper,</h3>
                 <div className="resume-container">
                     <a href={Resume} className="resumelink" target="_blank" rel="noopener noreferrer">
-                        Resume ↗
+                        Resume <span className="emoji">&#8599;</span>
                     </a>
                 </div>
                 <div className="footer__links">
-                    <a href="https://www.linkedin.com/in/alyssaxto" target="_blank" rel="noopener noreferrer">LinkedIn ↗</a>
-                    <a href="mailto:alyssaxto@gmail.com" target="_blank" rel="noopener noreferrer">Email ↗</a>
-                    <a href="https://github.com/alyssaxto" target="_blank" rel="noopener noreferrer">GitHub ↗</a>
+                    <a href="https://www.linkedin.com/in/alyssaxto" target="_blank" rel="noopener noreferrer">
+                        LinkedIn <span className="emoji">&#8599;</span>
+                    </a>
+                    <a href="mailto:alyssaxto@gmail.com" target="_blank" rel="noopener noreferrer">
+                        Email <span className="emoji">&#8599;</span>
+                    </a>
+                    <a href="https://github.com/alyssaxto" target="_blank" rel="noopener noreferrer">
+                        GitHub <span className="emoji">&#8599;</span>
+                    </a>
                 </div>
             </div>
         </footer>
