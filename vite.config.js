2b82d72e69e6a8657272e0ce006d264d139749d8
Alyssa To <142935267+alyssaxto@users.noreply.github.com>
2025-03-05 00:22:45 -0600

    changed minor things

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Make sure this matches your GitHub repo name
+  base: '', // Make sure this matches your GitHub repo name
   resolve: {
     alias: {
       '@': '/src',  // Ensures @ points to the src directory
