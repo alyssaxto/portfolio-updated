aa53090977fac29b99dc03e89221d3052aaa94b4
Alyssa To <142935267+alyssaxto@users.noreply.github.com>
2026-01-22 21:47:22 -0600

    removed the sides fro now

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,55 +2,35 @@ import { useEffect, useState } from "react";
 import "./hero.css";
 import heroImage from "/assets/magic.gif";
 import { TextFade } from "./Components/TextFade";
-import magicleft from "/assets/magicleft.gif";
-import magicright from "/assets/magicright.gif";
-
-
 
 const Hero = () => {
   const [offset, setOffset] = useState(0);
 
-
-
   return (
-<section className="hero" id="home">
-<TextFade direction="up" className="hero__content hero__text-wrapper">
-  <img
-    src={magicleft}
-    alt="Magic left"
-    className="hero__magic-overlay hero__magic-left"
-  />
-  <img
-    src={magicright}
-    alt="Magic right"
-    className="hero__magic-overlay hero__magic-right"
-  />
+    <section className="hero" id="home">
+      <TextFade direction="up" className="hero__content hero__text-wrapper">
+        <h4>Hey, I’m Alyssa</h4>
+        <p>
+          I’m <a href="#projects" className="highlight">designing</a> for and dreaming <br/>
+          of a world that’s a little more <a href="/playground" className="highlight">playful</a>.
+        </p>
+      </TextFade>
 
-  <h4>Hey, I’m Alyssa</h4>
-  <p>
-    I’m <a href="#projects" className="highlight">designing</a> for and dreaming <br/>
-    of a world that’s a little more <a href="/playground" className="highlight">playful</a>.
-  </p>
-</TextFade>
+      <img
+        src={heroImage}
+        alt="Alyssa on a bike"
+        className="hero__image"
+        style={{
+          transform: `translateX(${Math.min(0, -offset * 0.1)}px)`,
+        }}
+      />
 
-
-  <img
-    src={heroImage}
-    alt="Alyssa on a bike"
-    className="hero__image"
-    style={{
-      transform: `translateX(${Math.min(0, -offset * 0.1)}px)`,
-    }}
-  />
-
-  <TextFade direction="up" className="hero__content">
-    <p style={{ marginTop: "4rem" }}>
-      P.S. Click around the screen <span className="no-break">(˶ᵔ ᵕ ᵔ˶)ᯓ★</span>
-    </p>
-  </TextFade>
-</section>
-
-
+      <TextFade direction="up" className="hero__content">
+        <p style={{ marginTop: "4rem" }}>
+          P.S. Click around the screen <span className="no-break">(˶ᵔ ᵕ ᵔ˶)ᯓ★</span>
+        </p>
+      </TextFade>
+    </section>
   );
 };
 
